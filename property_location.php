fe8e0ad697d30d844c74e30f4230aebc911f53b9
dinesh2054 <90499283+dinesh2054@users.noreply.github.com>
2021-09-16 08:47:02 +1000

    Add files via upload

---
+++ property_location.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Properties Location</title>
+</head>
+<body>
+
+    <?php
+        session_start();
+        if($_SESSION['user_type']=="land owner"){
+
+            include "include/property_manager_navbar.php"; 
+        
+        }elseif($_SESSION['user_type']=="customer"){
+            
+            include "include/customer_navbar.php"; 
+        
+        }elseif($_SESSION['user_type']=="admin"){
+            
+            include "include/admin_navbar.php"; 
+        
+        }
+
+        $address= $_GET["address"];
+        ?>
+        <section class=" section-t4">
+            <div class="container">
+                    <div class="title-box-d">
+                        <h3 class="title-d">Location</h3>
+                    </div>  
+            </div> 
+            <div class="container">
+                <!-- https://www.google.com/maps/dir/Madan+Bhandari+Memorial+College,+Rudmatti+Marg,+Kathmandu/Jorpati/@27.7131374,85.3365023,14z/ -->
+                <!-- <iframe width="100%" height="500" src="https://www.google.com/maps/dir/Madan+Bhandari+Memorial+College,+Rudmatti+Marg,+Kathmandu/Jorpati/@27.7131374,85.3365023,14z/">
+                </iframe> -->
+                <iframe width="100%" height="500" src="https://maps.google.com/maps?q=<?php echo $address; ?>&output=embed">
+                </iframe>
+            </div>
+        </section>
+        <?php
+
+    ?> 
+</body>
+</html>
+
